942b2dc0649bc71976d4f77ee69599498a73a053
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2024-10-13 18:32:08 -0500

    Finalizando video nro 4

--- src/main/java/pe/edu/unfv/infrastructure/utils/ErrorCatalog.java
+++ src/main/java/pe/edu/unfv/infrastructure/utils/ErrorCatalog.java
@@ -10,7 +10,7 @@ public enum ErrorCatalog {
 	STUDENT_NOT_FOUND("STUDENT_MS_001", "Student not found."),
 	STUDENT_BAD_PARAMETERS("STUDENT_MS_002", "Invalid parameters for creation student."),
 	INTERNAL_SERVER_ERROR("STUDENT_MS_003", "Internal server error.");
-	
+		
 	private final String code;
 	private final String message;
 }
