fcf944492622529c8c2387e255c5d0ab25c99303
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-10-21 16:12:41 -0500

    Avanzando con el video nro 6 min 39.30

--- src/main/java/pe/edu/unfv/infrastructure/adapters/output/restclient/client/CourseFeignClient.java
+++ src/main/java/pe/edu/unfv/infrastructure/adapters/output/restclient/client/CourseFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "courses-services", url = "http://localhost:8087")
+@FeignClient(name = "courses-services", url = "http://localhost:8097")
 public interface CourseFeignClient {
 
 	@DeleteMapping("/courses/remove-student-from-collection/{studentId}")
