f2f2f4c89452e2361c7599964270fc95236c8ccc
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-10-25 11:58:59 -0500

    avanzando con el video nro 6 minuto 1:46

--- src/test/java/pe/edu/unfv/application/services/StudentServiceTest.java
+++ src/test/java/pe/edu/unfv/application/services/StudentServiceTest.java
@@ -1,16 +1,18 @@
 package pe.edu.unfv.application.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.ArgumentMatchers.anyIterable;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyCollection;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -23,6 +25,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import pe.edu.unfv.application.ports.output.ExternalCoursesOutputPort;
 import pe.edu.unfv.application.ports.output.StudentPersistencePort;
+import pe.edu.unfv.domain.exceptions.StudentEmailAlreadyExistsException;
 import pe.edu.unfv.domain.exceptions.StudentNotFoundException;
 import pe.edu.unfv.domain.models.Student;
 import pe.edu.unfv.utils.TestUtils;
@@ -32,36 +35,37 @@ class StudentServiceTest {
 
 	@Mock
 	private StudentPersistencePort studentPersistencePort;
-	
+
 	@Mock
-	private ExternalCoursesOutputPort externalCoursesOutputPort; 
-	
+	private ExternalCoursesOutputPort externalCoursesOutputPort;
+
 	@InjectMocks
 	private StudentService studentService;
-	
+
 	@Test
 	void testFindById_Success() {
-		
-		//Inicializacion
+
+		// Inicializacion
 		when(studentPersistencePort.findById(anyLong())).thenReturn(Optional.of(TestUtils.buildStudent()));
-		
-		//Evaluacion del comportamiento		
+
+		// Evaluacion del comportamiento
 		Student respuesta = studentService.findById(1L);
-		
-		//Comprobaciones o aserciones
+
+		// Comprobaciones o aserciones
 		assertNotNull(respuesta);
 		assertEquals(1L, respuesta.getId());
-		assertEquals("Pepito", respuesta.getFirstName());		
+		assertEquals("Pepito", respuesta.getFirstName());
 		verify(studentPersistencePort, times(1)).findById(1L);
 	}
-	
+
 	@Test
 	void testFindById_Throw() {
-		
-		//Inicializacion
-		when(studentPersistencePort.findById(anyLong())).thenReturn(Optional.empty());
-		
-		//Comprobaciones o aserciones		
+
+		// Inicializacion
+		Optional<Student> empty = Optional.empty();
+		when(studentPersistencePort.findById(anyLong())).thenReturn(empty);
+
+		// Comprobaciones o aserciones
 		assertThrows(StudentNotFoundException.class, () -> {
 			studentService.findById(4L);
 		});
@@ -69,38 +73,128 @@ class StudentServiceTest {
 	}
 
 	@Test
-	void testFindAll() {
-		when(studentPersistencePort.findByIds(anyIterable())).thenReturn(Collections.singletonList(TestUtils.buildStudent()));
+	void testFindByIds() {
 
-		List<Student> respuesta = studentPersistencePort.findByIds(Collections.singletonList(1L));
-		
+		// Inicializacion
+		List<Long> ids = Collections.singletonList(1L);
+		when(studentPersistencePort.findByIds(anyCollection()))
+				.thenReturn(Collections.singletonList(TestUtils.buildStudent()));
+
+		// Evaluacion del comportamiento
+		List<Student> respuesta = studentService.findByIds(Collections.singletonList(1L));
+
+		// Comprobaciones o aserciones
+		assertFalse(respuesta.isEmpty());
 		assertEquals(1, respuesta.size());
-		verify(studentPersistencePort, times(1)).findByIds(Collections.singletonList(1L));
+		assertEquals(1L, respuesta.get(0).getId());
+		assertEquals("Pepito", respuesta.get(0).getFirstName());
+		assertEquals("Dos Palotes", respuesta.get(0).getLastName());
+		verify(studentPersistencePort, times(1)).findByIds(ids);
 	}
 
+	@Test
+	void testFindAll() {
+		// Inicializacion
+		when(studentPersistencePort.findAll()).thenReturn(TestUtils.studentList());
+
+		// Evaluacion del comportamiento
+		List<Student> respuesta = studentService.findAll();
+
+		// Comprobaciones o aserciones
+		assertFalse(respuesta.isEmpty());
+		assertEquals(6, respuesta.size());
+		assertEquals(6L, respuesta.get(5).getId());
+		assertEquals("Juan", respuesta.get(1).getFirstName());
+		assertEquals("Coqueto", respuesta.get(3).getLastName());
+		verify(studentPersistencePort, times(1)).findAll();
+	}
+
+	@Test
+	void testSave_Success() {
+
+		// Inicializacion
+		Student studentToSave = Student.builder().id(1L).firstName("Pepito").lastName("Dos Palotes").age(15)
+				.email("pepito@gmail.com").address("Clle. 1").build();
+
+		when(studentPersistencePort.existsByEmail(anyString())).thenReturn(Boolean.FALSE);
+		when(studentPersistencePort.save(any(Student.class))).thenReturn(TestUtils.buildStudent());
+
+		// Evaluacion del comportamiento
+		Student respuesta = studentService.save(studentToSave);
+
+		// Comprobaciones o aserciones
+		assertNotNull(respuesta);
+		assertEquals(1L, respuesta.getId());
+		assertEquals("Pepito", respuesta.getFirstName());
+		assertEquals("Dos Palotes", respuesta.getLastName());
+		assertEquals(15, respuesta.getAge());
+		assertEquals("pepito@gmail.com", respuesta.getEmail());
+		assertEquals("Clle. 1", respuesta.getAddress());
+		verify(studentPersistencePort, times(1)).existsByEmail("pepito@gmail.com");
+		verify(studentPersistencePort, times(1)).save(studentToSave);
+	}
+
+	@Test
+	void testSave_Faild() {
+
+		// Inicializacion
+		Student studentToSave = Student.builder().id(1L).firstName("Pepito").lastName("Dos Palotes").age(15)
+				.email("pepito@gmail.com").address("Clle. 1").build();
+
+		when(studentPersistencePort.existsByEmail(anyString())).thenReturn(Boolean.TRUE);
+
+		// Comprobaciones o aserciones
+		assertThrows(StudentEmailAlreadyExistsException.class, () -> {
+			studentService.save(studentToSave);
+		});
+		verify(studentPersistencePort, times(1)).existsByEmail("pepito@gmail.com");
+		verify(studentPersistencePort, times(0)).save(studentToSave);
+	}
 	
-	// Then => entonces
-	//		assertThrows(IllegalArgumentException.class, () -> {
-	//			example.factorial(numero);
-	//		});
 	@Test
-	void testSave() {
-		fail("Not yet implemented");
+	void testUpdate_Success() {
+
+		// Inicializacion
+		Student studentToUpdate = Student.builder().id(1L).firstName("Pepito1").lastName("Dos Palotes1").age(15)
+				.email("pepito1@gmail.com").address("Clle. 11").build();
+
+		when(studentPersistencePort.existsByEmail(anyString())).thenReturn(Boolean.FALSE);
+		when(studentPersistencePort.findById(anyLong())).thenReturn(Optional.of(TestUtils.buildStudent()));
+
+		// Evaluacion del comportamiento
+		Student respuesta = studentService.update(1L, studentToUpdate);
+		
+		// Comprobaciones o aserciones
+				assertNotNull(respuesta);
+				assertEquals(1L, respuesta.getId());
+				assertEquals("Pepito", respuesta.getFirstName());
+				assertEquals("Dos Palotes", respuesta.getLastName());
+				assertEquals(15, respuesta.getAge());
+				assertEquals("pepito@gmail.com", respuesta.getEmail());
+				assertEquals("Clle. 1", respuesta.getAddress());
+				verify(studentPersistencePort, times(1)).existsByEmail("pepito@gmail.com");
+				//verify(studentPersistencePort, times(1)).save(studentToSave);
 	}
 
 	@Test
-	void testUpdate() {
-		fail("Not yet implemented");
+	void testUpdate_Fail() {
+
+		// Inicializacion
+		Student studentToUpdate = Student.builder().id(1L).firstName("Pepito").lastName("Dos Palotes").age(15)
+				.email("pepito@gmail.com").address("Clle. 1").build();
+
+		when(studentPersistencePort.existsByEmail(anyString())).thenReturn(Boolean.TRUE);
+
+		// Comprobaciones o aserciones
+		assertThrows(StudentEmailAlreadyExistsException.class, () -> {
+			studentService.save(studentToUpdate);
+		});
+		verify(studentPersistencePort, times(1)).existsByEmail("pepito@gmail.com");
+		verify(studentPersistencePort, times(0)).save(studentToUpdate);
 	}
 
 	@Test
 	void testDeleteById() {
 		fail("Not yet implemented");
 	}
-
-	@Test
-	void testFindByIds() {
-		fail("Not yet implemented");
-	}
-
 }
--- src/test/java/pe/edu/unfv/utils/TestUtils.java
+++ src/test/java/pe/edu/unfv/utils/TestUtils.java
@@ -1,5 +1,7 @@
 package pe.edu.unfv.utils;
 
+import java.util.List;
+
 import pe.edu.unfv.domain.models.Student;
 
 public class TestUtils {
@@ -9,8 +11,22 @@ public class TestUtils {
 				.id(1L)
 				.firstName("Pepito")
 				.lastName("Dos Palotes")
+				.age(15)
 				.email("pepito@gmail.com")
 				.address("Clle. 1")
 				.build();
+	}
+
+	public static List<Student> studentList() {
+
+		System.out.println(" -> Obteniendo listado estudiantes / Mock");
+
+		return List.of(
+				new Student(1L, "Pepito", "Dos Palotes", 15, "pepito@gmail.com", "Clle. 1"),
+				new Student(2L, "Juan", "Sin miedo", 10, "juan@gmail.com", "Clle. 2"),
+				new Student(3L, "Miguel", "Barraza", 65, "miguel@gmail.com", "Clle. 3"),
+				new Student(4L, "Santiago", "Coqueto", 11, "santiago@gmail.com", "Clle. 4"),
+				new Student(5L, "Carlitos", "Suero", 5, "carlitos@gmail.com", "Clle. 5"),
+				new Student(6L, "Alfredito", "Chichicuarima", 8, "alfredito@gmail.com", "Clle. 6"));
 	}
 }
