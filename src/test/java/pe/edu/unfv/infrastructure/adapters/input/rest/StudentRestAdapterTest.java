223f1f659f549463104eaf2bdd9af93716358843
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2024-10-29 00:15:07 -0500

    Avanzando con el video nro 7 minuto 2:12:00

--- src/test/java/pe/edu/unfv/infrastructure/adapters/input/rest/StudentRestAdapterTest.java
+++ src/test/java/pe/edu/unfv/infrastructure/adapters/input/rest/StudentRestAdapterTest.java
@@ -1,8 +1,5 @@
 package pe.edu.unfv.infrastructure.adapters.input.rest;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
