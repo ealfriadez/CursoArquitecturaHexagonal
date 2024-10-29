bb537b358abaa5d9f6f0d4c8cde47661823301c3
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-10-29 09:59:58 -0500

    Funcionando correctamente la vercion estable

---
+++ src/test/java/pe/edu/unfv/infrastructure/adapters/input/rest/StudentRestAdapterTest.java
@@ -0,0 +1,20 @@
+package pe.edu.unfv.infrastructure.adapters.input.rest;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(StudentRestAdapter.class)
+class StudentRestAdapterTest {
+
+	/*
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private StudentInputPort inputPort;
+
+	@MockBean
+	private StudentRestMapper restMapper;
+	
+	private ObjectMapper objectMapper;
+	*/
+}
